d3c25367a1b818c5b8fa1d9db2fa82d932e9051b
Bryan King <bpking15@gmail.com>
2025-09-20 09:23:00 -0400

    Enabling NuxtLayouts

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>nuxt-blog-starter</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
