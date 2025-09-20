a982f5fc67b04c249adef6cf13fd6acba3f837a5
Bryan King <bpking15@gmail.com>
2025-09-20 14:39:51 -0400

    Adding the Nuxt SEO modules to handle sitemaps, robots.txt, opengraph, etc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','@nuxt/content','@nuxt/fonts']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxtjs/seo'
+  ],
+  site: {
+    url: 'https://nuxtjs-blog-starter.vercel.app',
+    name: 'nuxt-blog-starter',
+    description: 'A barebones blog setup using Nuxt and Tailwind.',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  }
 })
