d308d6cc3a88c090290e98bc44e1d5dc774518d0
Bryan King <bpking15@gmail.com>
2025-09-20 09:58:13 -0400

    Adding some basic home page content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
+  <div class="mt-20">
+    <h1 class="text-2xl font-semibold tracking-tighter">My Portfolio</h1>
+    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </template>
 
