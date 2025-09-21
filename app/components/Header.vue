eccb24ece75882733d97242504694cd65e6b980a
Bryan King <bpking15@gmail.com>
2025-09-21 10:06:27 -0400

    Add an `external` prop so that we can set appropriate attributes on the external links

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <header class="pt-4">
     <nav class="flex gap-3">
-      <NuxtLink v-for="item in navItems" :to="item.path">{{ item.name }}</NuxtLink>
+      <NuxtLink
+        v-for="item in navItems"
+        :to="item.path"
+        :target="item.external ? '_blank' : undefined"
+        :rel="item.external ? 'noopener noreferrer' : undefined"
+      >
+        {{ item.name }}
+      </NuxtLink>
     </nav>
   </header>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 const navItems = {
   'home': {
     name: 'home',
@@ -18,9 +25,9 @@ const navItems = {
   },
   'deploy': {
     name: 'deploy',
-    path: 'https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fboldlybryan%2Fnuxt-blog-starter'
+    path: 'https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fboldlybryan%2Fnuxt-blog-starter',
+    external: true
   }
-
 }
 </script>
 
