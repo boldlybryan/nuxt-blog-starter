35b989fbaf525e2718201026e8b92ecd6e410a14
Bryan King <bpking15@gmail.com>
2025-09-21 08:35:46 -0400

    Styling updates to OG image

--- app/components/OgImage/OgImage.vue
+++ app/components/OgImage/OgImage.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="flex flex-col justify-between w-full h-full bg-gradient-to-br from-gray-900 to-gray-800 p-16">
-    <div class="flex flex-col">
-      <div class="text-5xl font-bold text-white mb-8 leading-tight">
-        {{ title }}
-      </div>
-      <div v-if="description" class="text-3xl text-gray-300 leading-relaxed">
-        {{ description }}
-      </div>
-    </div>
-    <div class="flex items-end justify-between">
+  <div class="flex flex-col justify-between gap-8 w-full h-full bg-gray-950 p-8">
+    <div class="flex">
       <div class="text-2xl text-gray-400">
         {{ siteName }}
       </div>
-      <div class="text-xl text-gray-500">
-        {{ date }}
+    </div>
+    <div class="flex flex-col items-end justify-between">
+      <div class="text-4xl font-bold text-white mb-2 leading-tight">
+        {{ title }}
+      </div>
+      <div v-if="description" class="text-2xl font-normal text-gray-300 leading-relaxed">
+        {{ description }}
       </div>
     </div>
   </div>
