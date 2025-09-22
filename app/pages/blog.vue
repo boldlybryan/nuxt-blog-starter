1ce710b8f053e5a0ccf8c9239b7216446979f281
Bryan King <bpking15@gmail.com>
2025-09-22 07:42:34 -0400

    match blog page to index page styling

--- app/pages/blog.vue
+++ app/pages/blog.vue
@@ -3,12 +3,12 @@
     <h1 class="text-2xl font-semibold tracking-tighter">Blog</h1>
   </div>
   <div class="mt-8">
-    <NuxtLink v-for="page in pages" :key="page.path" :to="page.path" class="flex align-baseline justify-between mb-6">
-      <div>
+    <NuxtLink v-for="page in pages" :key="page.path" :to="page.path" class="flex align-baseline justify-between gap-8 mb-6">
+      <div class="flex-1">
         <h2 class="font-semibold">{{ page.title }}</h2>
         <p class="opacity-60">{{ page.description }}</p>
       </div>
-      <div class="opacity-60">{{ page.date }}</div>
+      <NuxtTime :datetime="page.date" year="numeric" month="short" day="numeric" class="text-sm opacity-60"/>
     </NuxtLink>
   </div>
 </template>
